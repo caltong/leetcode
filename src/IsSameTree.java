53307270e1d83e54c0b8293aa09c0af2e24c216a
caltong <caltong1027@gmail.com>
2020-01-12 11:06:26 +0800

    Q100

---
+++ src/IsSameTree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
+
+public class IsSameTree {
+    public static void main(String[] args) {
+
+    }
+}
