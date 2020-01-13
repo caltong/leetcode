8eea1340e3c0ae22c5ff850e09b0a033dc73d6f0
caltong <caltong1027@gmail.com>
2020-01-13 17:56:47 +0800

    Q104

---
+++ src/MaxDepth.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
+
+public class MaxDepth {
+}
