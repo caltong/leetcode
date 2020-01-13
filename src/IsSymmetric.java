66ef1cc415f1429a61bdb11c66ffe0d1b1873245
caltong <caltong1027@gmail.com>
2020-01-13 17:14:30 +0800

    Q101

--- src/IsSymmetric.java
+++ src/IsSymmetric.java
@@ -7,15 +7,24 @@
  * TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+class Solution101 {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
             return true;
-        }
-        if (root.left == root.right) {
-            return isSymmetric(root.left) && isSymmetric(root.right);
         } else {
+            return isSymmetricSame(root.left, root.right);
+        }
+    }
+
+    boolean isSymmetricSame(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        } else if (t1 == null || t2 == null) {
             return false;
+        } else if (t1.val != t2.val) {
+            return false;
+        } else {
+            return isSymmetricSame(t1.left, t2.right) && isSymmetricSame(t1.right, t2.left);
         }
     }
 }
