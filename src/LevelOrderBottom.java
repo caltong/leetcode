a2089188d0352ad0c1cc7ede6c614f4b7fe91005
caltong <caltong1027@gmail.com>
2020-01-15 16:32:12 +0800

    Q107

---
+++ src/LevelOrderBottom.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> oneLevel = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode oneNode = queue.poll();
+                oneLevel.add(oneNode.val);
+                if (oneNode.left != null) {
+                    queue.add(oneNode.left);
+                }
+                if (oneNode.right != null) {
+                    queue.add(oneNode.right);
+                }
+            }
+            result.add(0, oneLevel);
+        }
+        return result;
+    }
+}
+
+public class LevelOrderBottom {
+}
