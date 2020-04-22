2715863e88b6db3e45ea202889012c75012fc127
caltong <caltong1027@gmail.com>
2020-04-22 13:32:15 +0800

    Q199

---
+++ src/RightSideView.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution199 {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> results = new ArrayList<>();
+        if (root == null) {
+            return results;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode subTree = queue.poll();
+                assert subTree != null;
+                if (subTree.left != null) {
+                    queue.add(subTree.left);
+                }
+                if (subTree.right != null) {
+                    queue.add(subTree.right);
+                }
+                if (i == size - 1) {
+                    results.add(subTree.val);
+                }
+            }
+        }
+        return results;
+    }
+}
+
+public class RightSideView {
+    public static void main(String[] args) {
+        Solution199 solution199 = new Solution199();
+        List<Integer> result = solution199.rightSideView(null);
+        System.out.println(result);
+    }
+}
