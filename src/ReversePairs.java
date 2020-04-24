835832e6e07a3952fe9040a6fb34ecdc5852002c
caltong <caltong1027@gmail.com>
2020-04-24 23:33:12 +0800

    Interview 51

---
+++ src/ReversePairs.java
@@ -0,0 +1,73 @@
+class SolutionInterview51 {
+    public int reversePairs(int[] nums) {
+        int len = nums.length;
+
+        if (len < 2) {
+            return 0;
+        }
+
+        int[] copy = new int[len];
+        System.arraycopy(nums, 0, copy, 0, len);
+
+        int[] temp = new int[len];
+        return reversePairs(copy, 0, len - 1, temp);
+    }
+
+    /**
+     * nums[left..right] 计算逆序对个数并且排序
+     */
+    private int reversePairs(int[] nums, int left, int right, int[] temp) {
+        if (left == right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+        int leftPairs = reversePairs(nums, left, mid, temp);
+        int rightPairs = reversePairs(nums, mid + 1, right, temp);
+
+        if (nums[mid] <= nums[mid + 1]) {
+            return leftPairs + rightPairs;
+        }
+
+        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+        return leftPairs + rightPairs + crossPairs;
+    }
+
+    /**
+     * nums[left..mid] 有序，nums[mid + 1..right] 有序
+     */
+    private int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+        if (right + 1 - left >= 0) System.arraycopy(nums, left, temp, left, right + 1 - left);
+
+        int i = left;
+        int j = mid + 1;
+
+        int count = 0;
+        for (int k = left; k <= right; k++) {
+
+            if (i == mid + 1) {
+                nums[k] = temp[j];
+                j++;
+            } else if (j == right + 1) {
+                nums[k] = temp[i];
+                i++;
+            } else if (temp[i] <= temp[j]) {
+                nums[k] = temp[i];
+                i++;
+            } else {
+                nums[k] = temp[j];
+                j++;
+                count += (mid - i + 1);
+            }
+        }
+        return count;
+    }
+}
+
+public class ReversePairs {
+    public static void main(String[] args) {
+        SolutionInterview51 solutionInterview51 = new SolutionInterview51();
+        int res = solutionInterview51.reversePairs(new int[]{7, 5, 6, 4});
+        System.out.println(res);
+    }
+}
