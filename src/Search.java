21c971498632197fe70ac9e69b9f70ab3a2a24b6
caltong <caltong1027@gmail.com>
2020-04-27 10:18:11 +0800

    Q33

---
+++ src/Search.java
@@ -0,0 +1,38 @@
+class Solution33 {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+public class Search {
+    public static void main(String[] args) {
+        Solution33 solution33 = new Solution33();
+        int res = solution33.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0);
+        System.out.println(res);
+    }
+}
