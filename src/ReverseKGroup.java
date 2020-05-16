d28041920ccf9c36cd346967688ea72e918da0a5
caltong <caltong1027@gmail.com>
2020-05-16 12:01:07 +0800

    Q25

---
+++ src/ReverseKGroup.java
@@ -0,0 +1,38 @@
+import utils.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode curr = head;
+        ListNode next;
+        dummy.next = head;
+        int length = 0;
+        while (head != null) {
+            length += 1;
+            head = head.next;
+        }
+        for (int i = 0; i < length / k; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                next = curr.next;
+                curr.next = next.next;
+                next.next = prev.next;
+                prev.next = next;
+            }
+            prev = curr;
+            curr = prev.next;
+        }
+        return dummy.next;
+    }
+}
+
+public class ReverseKGroup {
+}
