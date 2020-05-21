71506ee675025b41b8e60dc33caa4580d5e97972
caltong <caltong1027@gmail.com>
2020-05-21 11:49:43 +0800

    Q5

---
+++ src/LongestPalindrome.java
@@ -0,0 +1,39 @@
+class Solution5 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left;
+        int R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L -= 1;
+            R += 1;
+        }
+
+        return R - L - 1;
+    }
+}
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        Solution5 solution5 = new Solution5();
+        String res = solution5.longestPalindrome("babad");
+        System.out.println(res);
+    }
+
+}
