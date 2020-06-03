28201adfa81728a32583b02984669fab17fc3c4f
caltong <caltong1027@gmail.com>
2020-06-03 10:43:11 +0800

    Q837

---
+++ src/New21Game.java
@@ -0,0 +1,25 @@
+class Solution837 {
+    public double new21Game(int N, int K, int W) {
+        if (K == 0) {
+            return 1.0;
+        }
+        double[] dp = new double[K + W + 1];
+        for (int i = K; i <= N && i < K + W; i++) {
+            dp[i] = 1.0;
+        }
+        dp[K - 1] = 1.0 * Math.min(N - K + 1, W) / W;
+        for (int i = K - 2; i >= 0; i--) {
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+        }
+        return dp[0];
+    }
+}
+
+public class New21Game {
+    public static void main(String[] args) {
+        Solution837 solution837 = new Solution837();
+        double res = solution837.new21Game(21, 17, 10);
+        System.out.println(res);
+    }
+}
+
