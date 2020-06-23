bf8eb7485b8e26c94352dd38cd0877f0b4b5ff6a
caltong <caltong1027@gmail.com>
2020-06-23 12:27:53 +0800

    Q67

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -1,33 +1,21 @@
 class Solution67 {
     public String addBinary(String a, String b) {
-        int carry = 0;
-        int currentA = 0;
-        int currentB = 0;
-        StringBuilder result = new StringBuilder();
-        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0; i--, j--) {
-            if (i >= 0) {
-                currentA = Character.getNumericValue(a.charAt(i));
-            } else {
-                currentA = 0;
-            }
-            if (j >= 0) {
-                currentB = Character.getNumericValue(b.charAt(j));
-            } else {
-                currentB = 0;
-            }
-            if ((carry + currentA + currentB) >= 2) {
-                result.insert(0, (carry + currentA + currentB) % 2);
-                carry = 1;
-            } else if ((carry + currentA + currentB) < 2) {
-                result.insert(0, carry + currentA + currentB);
-                carry = 0;
-            }
+        StringBuilder ans = new StringBuilder();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
         }
-        if (carry == 1) {
-            return result.insert(0, 1).toString();
-        } else {
-            return result.toString();
+
+        if (carry > 0) {
+            ans.append('1');
         }
+        ans.reverse();
+
+        return ans.toString();
     }
 }
 
