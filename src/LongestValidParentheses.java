7eb1143580e9330ef173b8c5feb3143423c48576
caltong <caltong1027@gmail.com>
2020-07-04 13:13:42 +0800

    Q32

---
+++ src/LongestValidParentheses.java
@@ -0,0 +1,26 @@
+class Solution32 {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
+}
+
+public class LongestValidParentheses {
+    public static void main(String[] args) {
+        Solution32 solution32 = new Solution32();
+        int res = solution32.longestValidParentheses(")()())");
+        System.out.println(res);
+    }
+}
