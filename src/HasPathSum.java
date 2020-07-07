5fa608739b4466f1fd611f86f98bf587481fb828
caltong <caltong1027@gmail.com>
2020-07-07 11:38:43 +0800

    Q112

---
+++ src/HasPathSum.java
@@ -0,0 +1,25 @@
+import utils.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == sum;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
+
+public class HasPathSum {
+}
