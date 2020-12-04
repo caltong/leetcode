ff30fc9afaa4d3a33fb3057ebb8b99b8a58d674c
caltong <caltong1027@gmail.com>
2020-12-04 20:27:08 +0800

    Q659

---
+++ src/IsPossible.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution659 {
+    public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> endMap = new HashMap<>();
+        for (int x : nums) {
+            int count = countMap.getOrDefault(x, 0) + 1;
+            countMap.put(x, count);
+        }
+        for (int x : nums) {
+            int count = countMap.getOrDefault(x, 0);
+            if (count > 0) {
+                int prevEndCount = endMap.getOrDefault(x - 1, 0);
+                if (prevEndCount > 0) {
+                    countMap.put(x, count - 1);
+                    endMap.put(x - 1, prevEndCount - 1);
+                    endMap.put(x, endMap.getOrDefault(x, 0) + 1);
+                } else {
+                    int count1 = countMap.getOrDefault(x + 1, 0);
+                    int count2 = countMap.getOrDefault(x + 2, 0);
+                    if (count1 > 0 && count2 > 0) {
+                        countMap.put(x, count - 1);
+                        countMap.put(x + 1, count1 - 1);
+                        countMap.put(x + 2, count2 - 1);
+                        endMap.put(x + 2, endMap.getOrDefault(x + 2, 0) + 1);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+public class IsPossible {
+    public static void main(String[] args) {
+        Solution659 solution659 = new Solution659();
+        boolean possible = solution659.isPossible(new int[]{1, 2, 3, 4, 4, 5});
+        System.out.println(possible);
+    }
+}
