8e2f3641d9e289a8ec9bb8d14e85475584d8dbf1
caltong <caltong1027@gmail.com>
2021-03-19 15:52:08 +0800

    Q1603

---
+++ src/ParkingSystem.java
@@ -0,0 +1,29 @@
+class ParkingSystem1603 {
+    int[] park;
+
+    public ParkingSystem1603(int big, int medium, int small) {
+        park = new int[3];
+        park[0] = big;
+        park[1] = medium;
+        park[2] = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (park[carType - 1] > 0) {
+            park[carType - 1] -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
+
+
+public class ParkingSystem {
+}
