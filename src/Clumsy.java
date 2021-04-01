a3dda93e342276bcff81cc5a67d6ad2815f60f46
caltong <caltong1027@gmail.com>
2021-04-01 20:37:20 +0800

    Q1006

---
+++ src/Clumsy.java
@@ -0,0 +1,35 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class Solution1006 {
+    public int clumsy(int N) {
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(N);
+        N--;
+
+        int index = 0; // 用于控制乘、除、加、减
+        while (N > 0) {
+            if (index % 4 == 0) {
+                stack.push(stack.pop() * N);
+            } else if (index % 4 == 1) {
+                stack.push(stack.pop() / N);
+            } else if (index % 4 == 2) {
+                stack.push(N);
+            } else {
+                stack.push(-N);
+            }
+            index++;
+            N--;
+        }
+
+        // 把栈中所有的数字依次弹出求和
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        return sum;
+    }
+}
+
+public class Clumsy {
+}
