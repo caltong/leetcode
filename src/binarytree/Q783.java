8cc371bdb9cc9ebd7bd88ff01ed958f15820279d
caltong <caltong1027@gmail.com>
2021-04-13 11:23:05 +0800

    Q783

---
+++ src/binarytree/Q783.java
@@ -0,0 +1,53 @@
+package binarytree;
+
+import utils.Tree;
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class Q783 {
+    List<Integer> list;
+
+    public int minDiffInBST(TreeNode root) {
+        list = new ArrayList<>();
+        dfs(root);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            min = Math.min(min, list.get(i) - list.get(i - 1));
+        }
+        return min;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root != null) {
+            dfs(root.left);
+            list.add(root.val);
+            dfs(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Q783 q783 = new Q783();
+        TreeNode treeNode = Tree.buildTreeFromList(Arrays.asList(1, 0, 48, null, null, 12, 49));
+        int res = q783.minDiffInBST(treeNode);
+        System.out.println(res);
+    }
+}
--- src/utils/Tree.java
+++ src/utils/Tree.java
@@ -3,17 +3,17 @@ package utils;
 import java.util.List;
 
 public class Tree {
-    private TreeNode root;
-    private List<Integer> treeList;
+    private static TreeNode root;
+    private static List<Integer> treeList;
 
-    public TreeNode buildTreeFromList(List<Integer> treeList) {
-        this.treeList = treeList;
+    public static TreeNode buildTreeFromList(List<Integer> treeList) {
+        Tree.treeList = treeList;
         root = new TreeNode(treeList.get(0));
         subTreeNode(root, 0);
         return root;
     }
 
-    private void subTreeNode(TreeNode node, Integer index) {
+    private static void subTreeNode(TreeNode node, Integer index) {
         int maxIndex = treeList.size() - 1;
         int leftIndex = index * 2 + 1;
         int rightIndex = index * 2 + 2;
