fb89d15a26f6c244dc64ba04830a0dc66d29daf7
caltong <caltong1027@gmail.com>
2021-04-30 17:01:09 +0800

    Q137

---
+++ src/hash/Q137.java
@@ -0,0 +1,22 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Q137 {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+}
