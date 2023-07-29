3ace570f50263b2ced49e2362f8b0afe9f301fe5
caltong <caltong1027@gmail.com>
2023-07-29 13:39:46 +0800

    Q141

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,42 @@
+import utils.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+            if (fastPointer == slowPointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        boolean res = new Solution141().hasCycle(head);
+        System.out.println(res);
+    }
+}
