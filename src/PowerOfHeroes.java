2749da562326bee1a9c2dfbc41faa799bfcb14fa
caltong <caltong1027@gmail.com>
2023-08-01 12:43:11 +0800

    Q2681

---
+++ src/PowerOfHeroes.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution2681 {
+    public int sumOfPower(int[] nums) {
+        Arrays.sort(nums);
+        int dp = 0, preSum = 0;
+        int res = 0, mod = 1000000007;
+        for (int i = 0; i < nums.length; i++) {
+            dp = (nums[i] + preSum) % mod;
+            preSum = (preSum + dp) % mod;
+            res = (int) ((res + (long) nums[i] * nums[i] % mod * dp) % mod);
+            if (res < 0) {
+                res += mod;
+            }
+        }
+        return res;
+    }
+}
+
+public class PowerOfHeroes {
+}
